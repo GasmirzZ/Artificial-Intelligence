4a3e742f55d25130ce76fc8712292d41a9c93792
unknown <djordjevic.aleksandar@elfak.rs>
2025-12-04 04:16:07 +0100

    Prvi lab odradjen

---
+++ Lab1-MojZad.py
@@ -0,0 +1,14 @@
+def brojanje(recnik):
+    if type(recnik).__name__ != 'dict':
+        raise (TypeError("Parametar funkcije mora biti recnik"))
+    rec = {}
+    rez = []
+    for x in recnik.values():
+        tip = type(x).__name__
+        rec[tip] = rec.get(tip, 0) + 1
+    for x in rec.keys():
+        rez.append((x, rec[x]))
+    return rez
+
+
+print(brojanje({1: 4, 2: [2, 3], 3: [5, 6], 4: 'test', 5: 9, 6: 8}))
