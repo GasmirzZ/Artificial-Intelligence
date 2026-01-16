ccee365045436bdf296efd481ae6785dae34318f
unknown <djordjevic.aleksandar@elfak.rs>
2026-01-16 09:48:31 +0100

    Dopuna da igrac igra

--- Priprema Lab 4/Zad7.py
+++ Priprema Lab 4/Zad7.py
@@ -1,6 +1,7 @@
 from itertools import permutations
 
 grid = [[1, 0, 0], [0, 0, 0], [0, 0, 3]]
+player_grid = [[1, 0, 0], [0, 0, 0], [0, 0, 3]]
 fixed = {0: 1, 8: 3}
 empty = [i for i in range(9) if i not in fixed]
 for p in permutations([n for n in range(1, 10) if n not in fixed.values()]):
@@ -10,3 +11,17 @@ for p in permutations([n for n in range(1, 10) if n not in fixed.values()]):
         for row in grid:
             print(row)
         break
+i = 1
+while i != 8:
+    print()
+    player_grid[i//3][i % 3] = int(input("Unesite broj: "))
+    if not player_grid[i//3][i % 3] == grid[i//3][i % 3]:
+        print("Broj nije tacan, pokusajte opet:")
+        player_grid[i//3][i % 3] = 0
+    else:
+        i += 1
+
+    print("Uspesno ste resili problem!")
+    print("Resenje:")
+    for row in player_grid:
+        print(row)
